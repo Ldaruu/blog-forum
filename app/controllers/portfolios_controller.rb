4a1cb7598dee3f213daa74e3c1b4b63b17232c60
Laszlo Daru <daruu@hotmail.com>
2020-02-04 23:00:02 +0100

    feat(Implemented edit and update func)

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,6 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio, only: [:show, :edit, :update, :destroy]
+
   def index
     @portfolio_items = Portfolio.all
   end
@@ -20,11 +22,33 @@ class PortfoliosController < ApplicationController
       end
     end
   end
+  
+    def edit 
+    end
+
+    def update
+      respond_to do |format|
+        if  @portfolio_item.update(portfolio_params)
+          format.html { redirect_to portfolios_path, notice: 'Portfolio record was successfully updated.' }
+          format.json { render :show, status: :ok, location:  @portfolio_item }
+        else
+          format.html { render :edit }
+          format.json { render json:  @portfolio_item.errors, status: :unprocessable_entity }
+        end
+      end
+    end
+
+  
+
 
   private
- 
-  def portfolio_params
-    params.require(:portfolio).permit(:title, :subtitle, :body)
-  end
+  
+    def set_portfolio
+      @portfolio_item = Portfolio.find(params[:id])
+    end
+
+    def portfolio_params
+      params.require(:portfolio).permit(:title, :subtitle, :body)
+    end
   
 end
