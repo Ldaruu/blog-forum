c419c2fe1a59468ad982f5142797ac859c7582b6
Laszlo Daru <daruu@hotmail.com>
2020-02-04 21:55:16 +0100

    feat(Integrated new and create action for Portfolio)

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,4 +3,28 @@ class PortfoliosController < ApplicationController
     @portfolio_items = Portfolio.all
   end
 
+  def new
+    @portfoli_item = Portfolio.new
+  end
+
+  def create
+    @portfolio_item = Portfolio.new(portfolio_params)
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Portfolio Item was successfully created.' }
+        format.json { render :index, status: :created, location: @portfolio_item }
+      else
+        format.html { render :new }
+        format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+ 
+  def portfolio_params
+    params.require(:portfolio).permit(:title, :subtitle, :body)
+  end
+  
 end
