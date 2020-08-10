ec6d677216b5bd28ae0dcd64e8e7d7a2020ad9b1
Laszlo Daru <daruu@hotmail.com>
2020-08-10 17:44:16 +0200

    updated(seeds file)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+  eamil:"example@admin.com",
+  password:"123456",
+  password_confirmation:"123456",
+  name:"BoB Admin",
+  roles:"site_admin"
+)
+puts "Admin User Created"
+
+User.create!(
+  eamil:"example@example.com",
+  password:"123456",
+  password_confirmation:"123456",
+  name:"BoB Guest",
+)
+puts "Guest User Created"
 
 3.times do 
   topic = Topic.create!(
